9b5e7bfeaa5e0753c481d45c4d69dbb3fed71eba
csilv <c.silva3108@hotmail.com>
2023-05-05 17:42:14 -0300

    Definicao da entidade product

---
+++ src/main/java/com/product/pocproducts/model/Product.java
@@ -0,0 +1,29 @@
+package com.product.pocproducts.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private static final long serialVersionUID = -2899467848569731944L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Date validity;
+    @Column(nullable = false)
+    private Integer qtt;
+    @Column(nullable = false)
+    private BigDecimal value;
+}
