2b71e24e601f41936f96a5e9e33f20ce59989237
csilv <c.silva3108@hotmail.com>
2023-05-05 15:28:24 -0300

    Inicio poc produtos

---
+++ src/main/java/com/product/pocproducts/PocProductsApplication.java
@@ -0,0 +1,13 @@
+package com.product.pocproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/product/pocproducts/PocProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.pocproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
