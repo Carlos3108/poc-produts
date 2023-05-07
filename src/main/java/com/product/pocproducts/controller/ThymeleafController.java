fcbe1473219c2eb05e8762ad6d340ea3fa0a8b32
csilv <c.silva3108@hotmail.com>
2023-05-07 19:16:22 -0300

    Implementacao do redirecionamento do buttom cadastrar

--- src/main/java/com/product/pocproducts/controller/ThymeleafController.java
+++ src/main/java/com/product/pocproducts/controller/ThymeleafController.java
@@ -4,21 +4,20 @@ import com.product.pocproducts.service.IProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @AllArgsConstructor
 public class ThymeleafController {
     private IProductService iProductService;
-    @RequestMapping("/home")
-    public String homePage() {
-        return "home";
-    }
-    @GetMapping("/all")
+    @GetMapping("/home")
     public ModelAndView listAll() {
         ModelAndView modelAndView = new ModelAndView("home");
         modelAndView.addObject("products", iProductService.findAll());
         return modelAndView;
+    }
+    @GetMapping("form-create")
+    public String formCreate(){
+        return "form-create";
     }
 }
