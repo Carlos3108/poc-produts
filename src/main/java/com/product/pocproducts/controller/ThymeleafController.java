5f49fb4cb4e44772a560341550bdfb3f3a344cde
csilv <c.silva3108@hotmail.com>
2023-05-07 17:57:19 -0300

    Definicao de endpoint para lista de product na view

---
+++ src/main/java/com/product/pocproducts/controller/ThymeleafController.java
@@ -0,0 +1,24 @@
+package com.product.pocproducts.controller;
+
+import com.product.pocproducts.service.IProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@AllArgsConstructor
+public class ThymeleafController {
+    private IProductService iProductService;
+    @RequestMapping("/home")
+    public String homePage() {
+        return "home";
+    }
+    @GetMapping("/all")
+    public ModelAndView listAll() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("listProduct", iProductService.findAll());
+        return modelAndView;
+    }
+}
