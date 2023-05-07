87f4e8394607931b6e923aad7b6bab8e068fc9a3
csilv <c.silva3108@hotmail.com>
2023-05-07 20:45:35 -0300

    Implementacao do form update

--- src/main/java/com/product/pocproducts/controller/ThymeleafController.java
+++ src/main/java/com/product/pocproducts/controller/ThymeleafController.java
@@ -1,11 +1,17 @@
 package com.product.pocproducts.controller;
 
+import com.product.pocproducts.dto.ProductDTO;
 import com.product.pocproducts.service.IProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigDecimal;
 
 @Controller
 @AllArgsConstructor
@@ -22,10 +28,27 @@ public class ThymeleafController {
         return "form-create";
     }
 
-    @GetMapping("/update/{id}")
-    public ModelAndView update(@PathVariable("id") Long id){
+    @GetMapping("/form-update/{id}")
+    public ModelAndView formUpdate(@PathVariable("id") Long id){
         ModelAndView modelAndView = new ModelAndView("update");
         modelAndView.addObject("product", iProductService.findByID(id));
         return modelAndView;
+    }
+
+    @GetMapping("/update")
+    public String update(@RequestParam Long id,
+                               @RequestParam String name,
+                               @RequestParam String validity,
+                               @RequestParam Integer qtt,
+                               @RequestParam BigDecimal value) {
+        ProductDTO productDTO = ProductDTO.builder()
+                .id(id)
+                .name(name)
+                .validity(validity)
+                .qtt(qtt)
+                .value(value)
+                .build();
+        iProductService.update(productDTO);
+        return "home";
     }
 }
